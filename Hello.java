811731ed8f8f79d81cd856a4432e20f5c9b285a6
gurudevops1234 <60510501+gurudevops1234@users.noreply.github.com>
2020-02-20 11:58:48 +0530

    new change

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ public class Hello
 {
 public static void main(String agrs[])
 {
+  int a=12,b=12b,c;
+  c= a+b;
+ System.out.println(" c is "+c);
 System.out.println("Hello to GIT");
   System.out.printlnn("Saiprasad");
   System.out.println("fathername");
